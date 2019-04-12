ac8aaf75c6f146b62e14730e2da89ef321d4b1df
Nathan Tolley <nathantolley@ip0af50dc8.int.uvm.edu>
2019-04-12 11:38:38 -0400

    Working algorithm is finished. Pretty sure it's running in O(n^2)

--- buildStructure.py
+++ buildStructure.py
@@ -1,22 +1,44 @@
-import numpy
+import numpy as np
 import random
 import math
-
-#Create a dictionary to store cube location data
-#Each cube is represented by a key with its location in 3D space, and a set of values that indicate the locations/keys of its children.
-#Until there are as many cubes as needed:
-#Pick a random cube
-#Compute a random space next to it by randomly adding or subtracting 1 from one of the three coordinates
-#Check the new coordinate against the keys. If it is filled, pick a new cube and try again.
 
 polycube = {}
 polycube[(0,0,0)] = []
 
+#Takes numcubes as an integer argument for the size of the desired polycube
+#Will randomly generate a polycube of desired size recursively
 def Add_Cube(numCubes):
 
-    if (numCubes > len(polycube)):
+    if(numCubes > len(polycube)):
 
+        #select whether to move + or -
         direction = random.randint(0, 1)*2 - 1
+
+        #coordinate to move in: 0->x, 1->y, 2->z
         coord = random.randint(0,2)
 
+        #pick a random cube from the structure
+        parent = random.choice(list(polycube.keys()))
+
+        #convert parent to a list, change the selected coordinate, and convert back to tuple
+        child = list(parent)
+        child[coord] += direction
+        child = tuple(child)
+
+        #if child's coordinates are already occupied, do that again
+        while(child in polycube.keys()):
+
+            parent = random.choice(list(polycube.keys()))
+            
+            child = list(parent)
+            child[coord] += direction
+            child = tuple(child)
         
+        #Point parent to child, add child to the structure
+        polycube[parent].append(child)
+        polycube[child] = []
+
+        Add_Cube(numCubes)
+
+Add_Cube(10)
+print(polycube)
