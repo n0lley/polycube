241ac4de91f5599c1efbd088b24794a0251a2e77
Nathan Tolley <nathantolley@65-183-130-151-dhcp.burlingtontelecom.net>
2019-04-13 15:33:12 -0400

    code to add basic joints written. Can modify later.

--- robot.py
+++ robot.py
@@ -5,13 +5,38 @@ import pyrosim
 import constants as c
 from aggregate import AGGREGATE
 
-polybot = AGGREGATE(c.NUMCUBES)
+polybot = AGGREGATE(50)
 print (polybot.polycube)
 
 sim = pyrosim.Simulator( play_paused=True )
 
+lowest = 0
 for coord in polybot.polycube:
-    sim.send_box(x=coord[0]*c.SCALE, y=coord[1]*c.SCALE, z=(coord[2]+c.NUMCUBES/2)*c.SCALE, length=c.SCALE, width=c.SCALE, height=c.SCALE, r=random.random(), g=random.random(), b=random.random())
+    if(coord[2] < lowest):
+        lowest = coord[2]
+
+cubes = {}
+
+for coord in polybot.polycube:
+    cubes[coord] = sim.send_box(
+                 x=coord[0]*c.SCALE, y=coord[1]*c.SCALE, z=(coord[2] - lowest + .5)*c.SCALE,
+                 length=c.SCALE, width=c.SCALE, height=c.SCALE,
+                 r=random.random(), g=random.random(), b=random.random()
+                 )
+
+joints = {}
+j=0
+for coord in polybot.polycube:
+    for neighbor in polybot.polycube[coord]:
+        xloc = (coord[0] - neighbor[0])/2
+        yloc = (coord[1] - neighbor[1])/2
+        zloc = (coord[2] - neighbor[2])/2
+        joints[j] = sim.send_hinge_joint(
+                    first_body_id = cubes[coord], second_body_id = cubes[neighbor],
+                    x=xloc*c.SCALE, y=yloc*c.SCALE, z=zloc*c.SCALE,
+                    n1=0, n2=0, n3=1,
+                    lo=-.0001, hi=.0001
+    )
 
 sim.start()
 
