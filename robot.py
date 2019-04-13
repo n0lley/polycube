26c940b12cf108540fbfb8e41811b75dbd7c5b81
Nathan Tolley <nathantolley@65-183-130-151-dhcp.burlingtontelecom.net>
2019-04-12 22:48:32 -0400

    test environment for classes and functions

--- robot.py
+++ robot.py
@@ -1,7 +1,13 @@
 import numpy as np
 import random
 import math
+import pyrosim
 from aggregate import AGGREGATE
 
 polybot = AGGREGATE(10)
 print (polybot.polycube)
+
+sim = pyrosim.Simulator( play_paused=True )
+
+sim.start()
+
