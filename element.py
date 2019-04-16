830480cf78f319a5ee798cc9cdb81c0e67be407d
Nathan Tolley <nathantolley@Slap-top.local>
2019-04-15 20:39:42 -0400

    created new class file for Element

---
+++ element.py
@@ -0,0 +1,16 @@
+"""
+    TODO: Create Element init which creates the Element in the sim
+    
+    add_neighbor which attaches child Elements via joint
+    
+    Mutate which changes the motor/sensors
+    """
+
+
+class ELEMENT:
+
+    def __init__(self, sim, coords):
+    
+    
+    def Mutate():
+
