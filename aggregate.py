71a5df8e7983556b301150ed6c9ec21c87ae4243
Nathan Tolley <nathantolley@ip0af50572.int.uvm.edu>
2019-04-17 11:04:56 -0400

    completed aggregate's send_to_sim method, save for details involving how element deals with sensors and motors

--- aggregate.py
+++ aggregate.py
@@ -5,15 +5,11 @@ import constants as c
 
 class AGGREGATE:
     
-    def __init__(self, sim, elements, numCubes, no_sim=False):
+    def __init__(self, numCubes):
         self.tree = {}
         self.tree[(0,0,0)] = []
         
         self.Build_Tree(numCubes)
-
-        if not no_sim:
-            self.body = {}
-            self.Build_Body(sim, elements)
 
     #Takes numcubes as an integer argument for the size of the desired polycube
     #Will randomly generate a polycube of desired size recursively
@@ -51,8 +47,10 @@ class AGGREGATE:
             self.Build_Tree(numCubes)
 
     #Construct a body using the provided body tree and randomly choosing block types from the provided list of elements
-    def Build_Body(self, sim, elements):
-
+    def send_to_sim(self, sim, elements):
+        
+        self.body = {}
+        
         #establish what the lowest z-coordinate in the tree is so the robot can be shifted up accordingly
         lowest = 0
         for coord in self.tree:
@@ -63,14 +61,102 @@ class AGGREGATE:
         for coord in self.tree:
             element = random.choice(elements)
             
-            self.Add_Cube(sim, element, coord)
+            #add a block
+            self.body[coord] = [self.Add_Cube(sim, element, coord, lowest), element]
 
-            
+        print(self.body)
+                
+        #add joints, motors, sensors to each box
+        for coord in self.tree:
+            joints = self.Build_Joints(sim, coord)
+            sensors, motors = self.Add_Neurons(sim, coord, joints)
+            self.Build_Network(sim, sensors, motors)
+
 
     #Create a cube at the specified coordinate, with the specified element's properties
-    def Add_Cube(self, sim, element, coord):
+    def Add_Cube(self, sim, element, coord, lowest):
         box = sim.send_box(
                            x=coord[0]*c.SCALE, y=coord[1]*c.SCALE, z=(coord[2] - lowest + .5)*c.SCALE,
                            length = element.size, width=element.size, height=element.size,
                            r=random.random(), g=random.random(), b=random.random()
                            )
+        return box
+
+    #use child list from tree and corresponding boxes in body to attach joints
+    def Build_Joints(self, sim, coord):
+
+        box = self.body[coord][0]
+        children = self.tree[coord]
+        
+        joints = {}
+        
+        j = 0
+        for child in children:
+            #box to be connected
+            box2 = self.body[child][0]
+            
+            #calculate joint's position
+            jx = (child[0] - coord[0])/2 + coord[0]
+            jy = (child[1] - coord[1])/2 + coord[1]
+            jz = (child[2] - coord[2])/2 + coord[2]
+
+            if( coord[0] == child[0] ):
+            #same x-coordinates, create joint with normal on x axis
+                joints[j] = sim.send_hinge_joint(
+                                                first_body_id = box, second_body_id = box2,
+                                                 x=jx, y=jy, z=jz,
+                                                 n1=1, n2=0, n3=0,
+                                                 lo=-1*math.pi/2., hi=math.pi/2.
+                                                 )
+                j+=1
+            
+            if( coord[1] == child[1] ):
+            #same y-coordinates, create joint with normal on y axis
+                joints[j] = sim.send_hinge_joint(
+                                                 first_body_id = box, second_body_id = box2,
+                                                 x=jx, y=jy, z=jz,
+                                                 n1=0, n2=1, n3=0,
+                                                 lo=-1*math.pi/2., hi=math.pi/2.
+                                                 )
+                j+=1
+            
+            if( coord[2] == child[2] ):
+            #same z-coordinates, create joint with normal on y axis
+                joints[j] = sim.send_hinge_joint(
+                                 first_body_id = box, second_body_id = box2,
+                                 x=jx, y=jy, z=jz,
+                                 n1=0, n2=0, n3=1,
+                                 lo=-1*math.pi/2., hi=math.pi/2.
+                                 )
+                j+=1
+
+        return joints
+
+    #add sensor and motor neurons per the box's element specifications
+    def Add_Neurons(self, sim, coord, joints):
+    
+        box = self.body[coord][0]
+        element = self.body[coord][1]
+    
+        sensors = {}
+        i=0
+        for s in element.sensors:
+            sensors[i] = s
+            i+=1
+        #placeholder until specifics of element's sensors are worked out
+
+        motors = {}
+        i=0
+        for m in element.motors:
+            motors[i] = m
+            i+=1
+        #placeholder until specifics of element's motors are worked out
+
+        return sensors, motors
+    
+    #build the synapses using the element's Controller
+    def Build_Network(self, sim, sensors, motors):
+
+        for s in sensors:
+            for m in motors:
+                sim.send_synapse(source_neuron_id = sensors[s], target_neuron_id = motors[m])
--- robot.py
+++ robot.py
@@ -10,46 +10,11 @@ sim = pyrosim.Simulator( play_paused=True )
 
 sensors = {}
 motors = {}
+controller = np.random.random((1, 12))
 
-element = ELEMENT(sensors, motors)
-polybot = AGGREGATE(sim, [element], 15)
-print (polybot.tree)
+element = ELEMENT(sensors, motors, controller)
+polybot = AGGREGATE(30)
+polybot.send_to_sim(sim, [element])
 
 sim.start()
 sim.wait_to_finish()
-"""
-joints1 = {}
-joints2 = {}
-joints3 = {}
-for coord in polybot.polycube:
-    for neighbor in polybot.polycube[coord]:
-        joints1[coord] = {}
-        joints2[coord] = {}
-        joints3[coord] = {}
-        xdif = (coord[0] - neighbor[0])/2
-        ydif = (coord[1] - neighbor[1])/2
-        zdif = (coord[2] - neighbor[2])/2
-        
-        if(xdif == 0):
-            joints1[coord][neighbor] = sim.send_hinge_joint(
-                        first_body_id = cubes[coord], second_body_id = cubes[neighbor],
-                        x=(xdif + coord[0])*c.SCALE, y=(ydif + coord[1])*c.SCALE, z=(zdif + coord[2])*c.SCALE,
-                        n1=1, n2=0, n3=0,
-                        lo=-math.pi/2., hi=math.pi/2.
-                        )
-        if(ydif == 0):
-            joints2[coord][neighbor] = sim.send_hinge_joint(
-                        first_body_id = cubes[coord], second_body_id = cubes[neighbor],
-                        x=(xdif + coord[0])*c.SCALE, y=(ydif + coord[1])*c.SCALE, z=(zdif + coord[2])*c.SCALE,
-                        n1=0, n2=1, n3=0,
-                        lo=-math.pi/2., hi=math.pi/2.
-                        )
-
-        if(zdif == 0):
-            joints3[coord][neighbor] = sim.send_hinge_joint(
-                        first_body_id = cubes[coord], second_body_id = cubes[neighbor],
-                        x=(xdif + coord[0])*c.SCALE, y=(ydif + coord[1])*c.SCALE, z=(zdif + coord[2])*c.SCALE,
-                        n1=0, n2=0, n3=1,
-                        lo=-math.pi/2., hi=math.pi/2.
-                        )
-"""
