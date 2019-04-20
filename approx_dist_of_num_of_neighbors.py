4ff161b0dbe4c7d49620e484318e691223e1e5a9
David Matthews <david.matthews.1@uvm.edu>
2019-04-20 01:06:28 -0400

    WIP on distinguishing between external and internal air to determine ratio of external to internal cubes

--- approx_dist_of_num_of_neighbors.py
+++ approx_dist_of_num_of_neighbors.py
@@ -1,33 +1,257 @@
 # todo: approximate (using Monte-Carlo method) and plot the distribution of number_of_neighbors as a function of the number_of_elements
 
+import queue
+import random
 import sys
 from multiprocessing import Pool
-import random
+
 import numpy as np
 
-
 from aggregate import AGGREGATE
-import pyrosim
 
-debug = False
+debug = True
+BLOCK = 1
+AIR = 2
 
-def get_neighbor_counts(node_count):
+
+class Air(object):
+    AIR_UNKOWN = 1
+    AIR_EXTERNAL = 2
+    AIR_INTERNAL = 3
+
+    def __init__(self, position):
+        self.position = position  # a tuple of (x,y,z)
+        self.type = Air.AIR_UNKOWN
+
+
+class Block(object):
+    def __init__(self, position, connections):
+        self.position = position  # a tuple of (x,y,z)
+        self.connections = connections  # a list of tuples of (x,y,z) positions of neighboring cubes
+        self.connection_count = len(self.connections)
+
+
+def get_aggregate(node_cnt):
+    return AGGREGATE(None, [None], node_cnt, no_sim=True).tree
+
+
+def compute_interior_nodes(node_cnt):
+    # get the structure
+    structure = get_aggregate(node_cnt)
+
+    # find bounding box
+    x_max_node = None
+    x_min, x_max = 0, 0
+    y_min, y_max = 0, 0
+    z_min, z_max = 0, 0
+
+    for x, y, z in structure:
+        # print(x,y,z)
+        if x < x_min:
+            x_min = x
+        elif x >= x_max:
+            x_max = x
+            x_max_node = (x, y, z)
+
+        if y < y_min:
+            y_min = y
+        elif y > y_max:
+            y_max = y
+
+        if z < z_min:
+            z_min = z
+        elif z > z_max:
+            z_max = z
+    # print(x_min, x_max)
+    # print(y_min, y_max)
+    # print(z_min, z_max)
+
+    # calc size of matrix
+    x_size = x_max - x_min + 3
+    y_size = y_max - y_min + 3
+    z_size = z_max - z_min + 3
+
+    # compute offsets
+    x_offset = -1 * x_min + 1
+    y_offset = -1 * y_min + 1
+    z_offset = -1 * z_min + 1
+
+    # reserve matrix
+    matrix = np.zeros(shape=(x_size, y_size, z_size), dtype=object)
+
+    node_queue_1 = queue.Queue()  # queue of Block Objects
+    node_queue_2 = queue.Queue()  # queue of Block Objects
+
+    air_queue = queue.Queue()  # queue of Air Objects
+
+    to_evaluate_queue = queue.Queue()  # queue of tuples of coordinates
+
+    # Add connection to parent node for each node. Runs in O(36n)=O(n) time
+    for node in structure:
+        for child in structure[node]:  # max of 6 children
+            if node not in structure[child]:  # list so traverse; max of 6 connections.
+                structure[child].append(node)
+
+    # fill in the matrix with nodes & fill queues with nodes
+    for x, y, z in structure:
+        node = (x + x_offset, y + y_offset, z + z_offset)
+        # print(node)
+        tmp = Block(node, structure[(x,y,z)])
+        matrix[node] = (BLOCK, tmp)
+        node_queue_1.put(node)  # used to construct neighboring air search space
+        node_queue_2.put(tmp)  # used to determine interior nodes
+
+    # Add air nodes to matrix + fill air_queue
+    while not node_queue_1.empty():
+        node = node_queue_1.get()
+        x, y, z = node
+
+        # add neighboring cells to air queue if they are empty and valid cells.
+        # x neighbors
+        for i in [-1, 0, 1]:
+            if x + i < 0 or x + i >= x_size:
+                continue
+            if matrix[x + i, y, z] != 0:
+                continue
+            # create new Air Object + add to queue and matrix
+            tmp_air = Air((x+i, y, z))
+            air_queue.put(tmp_air)
+            matrix[(x+i, y, z)] = (AIR, tmp_air)
+
+        # y neighbors
+        for i in [-1, 0, 1]:
+            if y + i < 0 or y + i >= y_size:
+                continue
+            if matrix[x, y + i, z] != 0:
+                continue
+            # create new Air Object + add to queue and matrix
+            tmp_air = Air((x, y + i, z))
+            air_queue.put(tmp_air)
+            matrix[(x, y + i, z)] = (AIR, tmp_air)
+
+        # z neighbors
+        for i in [-1, 0, 1]:
+            if z + i < 0 or z + i >= z_size:
+                continue
+            if matrix[x, y, z + i] != 0:
+                continue
+            # create new Air Object + add to queue and matrix
+            tmp_air = Air((x, y, z + i))
+            air_queue.put(tmp_air)
+            matrix[(x, y, z + i)] = (AIR, tmp_air)
+
+    # determine which node is guarenteed to be an exterior node. Start here
+    x, y, z = x_max_node
+    node = (x + x_offset+1, y + y_offset, z + z_offset)
+    # print("shape", matrix.shape)
+    # print(x_max_node)
+    # print(node)
+    tmp_air = matrix[node]
+    tmp_air[1].type = Air.AIR_EXTERNAL
+    to_evaluate_queue.put(tmp_air[1])
+
+    # flood local air to determine which cells are external air
+    while not to_evaluate_queue.empty():
+        curr_cell = to_evaluate_queue.get()
+        x, y, z = curr_cell.position
+
+        # x neighbors
+        for i in [-1, 1]:
+            if x + i < 0 or x + i >= x_size:
+                continue
+            cell = matrix[x + i, y, z]
+            if cell == 0 or cell[0] == BLOCK:
+                continue
+            cell[1].type = Air.AIR_EXTERNAL
+            to_evaluate_queue.put(cell[1])
+
+        # y neighbors
+        for i in [-1, 1]:
+            if y + i < 0 or y + i >= y_size:
+                continue
+            cell = matrix[x, y + i, z]
+            if cell == 0 or cell[0] == BLOCK:
+                continue
+
+            cell[1].type = Air.AIR_EXTERNAL
+            to_evaluate_queue.put(cell[1])
+
+        # z neighbors
+        for i in [-1, 1]:
+            if z + i < 0 or z + i >= x_size:
+                continue
+            cell = matrix[x, y, z + i]
+            if cell == 0 or cell[0] == BLOCK:
+                continue
+            cell[1].type = Air.AIR_EXTERNAL
+
+            to_evaluate_queue.put(cell[1])
+
+    node_neighbor_cnt_queue = queue.Queue()
+
+    while not node_queue_2.empty():
+        curr_cell = node_queue_2.get()
+        x, y, z = curr_cell.position
+
+        # x neighbors
+        for i in [-1, 1]:
+            if x + i < 0 or x + i >= x_size:
+                continue
+            cell = matrix[x + i, y, z]
+            if debug:
+                assert cell != 1, "Invalid cell type."
+            if cell[0] == AIR and cell[1].type != Air.AIR_EXTERNAL:
+                curr_cell.connection_count += 1
+
+        # y neighbors
+        for i in [-1, 1]:
+            if y + i < 0 or y + i >= y_size:
+                continue
+            cell = matrix[x, y + i, z]
+            if debug:
+                assert cell != 1, "Invalid cell type."
+            if cell[0] == AIR and cell[1].type != Air.AIR_EXTERNAL:
+                curr_cell.connection_count += 1
+
+        # z neighbors
+        for i in [-1, 1]:
+            if z + i < 0 or z + i >= x_size:
+                continue
+            cell = matrix[x, y, z + i]
+            if debug:
+                assert cell != 1, "Invalid cell type."
+            if cell[0] == AIR and cell[1].type != Air.AIR_EXTERNAL:
+                curr_cell.connection_count += 1
+
+        node_neighbor_cnt_queue.put(curr_cell.connection_count)
+
+    neighbor_frequencies = np.zeros(shape=6)
+    while not node_neighbor_cnt_queue.empty():
+        neighbor_cnt = node_neighbor_cnt_queue.get()
+        if neighbor_cnt == 0:
+            continue
+        neighbor_frequencies[neighbor_cnt - 1] += 1
+
+    return neighbor_frequencies
+
+
+def get_neighbor_counts(node_cnt):
     # generate aggregate
-    aggregate = AGGREGATE(None, [None], node_count, no_sim=True)
-    structure = aggregate.tree
+    structure = get_aggregate(node_cnt)
 
     # remove the origin. Each node has 1 parent except the origin which has none.
     origin = structure.pop((0, 0, 0))
 
     # calculate neighbor counts
+    # each node has 1 parent with the exception of the origin which has 0 parents.
     neighbor_counts = [len(d) + 1 for d in
-                       aggregate.tree.values()]  # each node has 1 parent with the exception of the origin which has 0 parents.
+                       structure.values()]
     # add the origin back in
     neighbor_counts.append(len(origin))
 
     # if in debug, assert truth values
     if debug:
-        if node_count > 1:
+        if node_cnt > 1:
             assert (min(neighbor_counts) > 0), "Error with generating polycube"
         else:
             assert (min(neighbor_counts) >= 0), "Error with generating polycube"
@@ -35,46 +259,57 @@ def get_neighbor_counts(node_count):
 
     # calculate neighbor frequencies
     neighbor_frequencies = np.zeros(shape=6)
-    for n in range(6):
-        neighbor_frequencies[n] = neighbor_counts.count(n + 1)
+    # print(neighbor_counts)
+    for n in neighbor_counts:
+        if n == 0:
+            continue
+        neighbor_frequencies[n - 1] += 1
     return neighbor_frequencies
 
-def compute_work( node_count, iteration_number, ttl_iterations,seed_offset=0):
+
+def compute_work(node_cnt, iteration_number, ttl_iteration_cnt, seed_offset=0):
     """
     wrapper method for get_neighbor_counts which also sets the seed + keeps track of which iteration this is.
-    :param node_count: number of nodes to include in our polynode
+    :param node_cnt: number of nodes to include in our polynode
     :param iteration_number: which iteration we are running
+    :param ttl_iteration_cnt: total number of iterations we plan to run. Needed for seeding calculations
     :param seed_offset: how to adjust the seed.
-    :return: tuple of node_count, iteration_number, and the frequency of num neighbors
+    :return: tuple of node_cnt, iteration_number, and the frequency of num neighbors
     """
     # TODO: Decide if this seeding is good or not.
-    random.seed(seed_offset + iteration_number + node_count*ttl_iterations)
-    ret = get_neighbor_counts(node_count)
-    return (node_count, iteration_number, ret)
+    random.seed(seed_offset + iteration_number + node_cnt * ttl_iteration_cnt)
+    neighbor_frequencies = compute_interior_nodes(node_cnt)
+    return node_cnt, iteration_number, neighbor_frequencies
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) >= 3, "Please run as python NAME.py <Number of nodes in polycube> <number of iterations for Monte-Carlo> <optional: starting seed>"
-    ttl_iterations = int(sys.argv[2])
+    # compute_interior_nodes(10)
+    # exit(0)
+    assert len(
+        sys.argv) >= 3, "Please run as python NAME.py <Number of nodes in polycube>" \
+                        "<number of iterations for Monte-Carlo> <optional: starting seed>"
+    num_iterations = int(sys.argv[2])
     ttl_node_count = int(sys.argv[1])
     if len(sys.argv) == 4:
-        seed_offset = int(sys.argv[3])
+        global_seed_offset = int(sys.argv[3])
     else:
-        seed_offset = 0
+        global_seed_offset = 0
 
-    data = np.zeros(shape=(ttl_node_count, ttl_iterations, 6))
+    np.set_printoptions(suppress=True, formatter={'float_kind': lambda x: '%5.2f' % x})
+
+    data = np.zeros(shape=(ttl_node_count, num_iterations, 6))
 
     # used for parallel computation
     process_count = None
     pool = Pool(processes=process_count)
 
-
     # compute the work
-    for j in range(ttl_node_count):
-        sys.stdout.write("%3d"%j)
+    for j in range(1, ttl_node_count):
+        sys.stdout.write("%3d" % j)
         sys.stdout.flush()
-        iterations_list = list(range(ttl_iterations))
-        res = [pool.apply_async(compute_work, args=( j, iter_num, ttl_iterations, seed_offset)) for iter_num in iterations_list]
+        iterations_list = list(range(num_iterations))
+        res = [pool.apply_async(compute_work, args=(j, iter_num, num_iterations, global_seed_offset)) for iter_num in
+               iterations_list]
         for r in res:
             node_count, iter_num, ret = r.get()
             data[node_count, iter_num] = ret
@@ -82,10 +317,4 @@ if __name__ == "__main__":
         sys.stdout.flush()
     pool.close()
     pool.join()
-    print(data)
-
-
-
-
-
-
+    print(data.mean(axis=1))
