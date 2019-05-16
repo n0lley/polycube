a178a911892d4d01ca3cc88ee3c7ba3dbf05f239
jack <jfelag@uvm.edu>
2019-05-16 13:13:44 -0400

    testClass -> template for population.py

---
+++ testClass.py
@@ -0,0 +1,24 @@
+
+
+class TEST:
+    
+    def __init__(self):
+        
+        self.fitness = 0
+            
+    
+    def mutate(self):
+        
+        pass
+
+    
+    def evaluate(self):
+        
+        pass
+    
+    
+    def generate_random(self):
+        
+        pass
+    
+    
