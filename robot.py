07f68bdeba8be744176370447cf10070d35f85b4
Nathan Tolley <nathantolley@65-183-148-99-dhcp.burlingtontelecom.net>
2019-05-18 12:57:58 -0400

    updated sim calls to reflect most recent pyrosim version

--- robot.py
+++ robot.py
@@ -4,14 +4,14 @@ import math
 import pyrosim
 import constants as c
 from aggregate import AGGREGATE
-from element import ELEMENT, TouchSensorHingeJointElement
+from element import ELEMENT, TouchSensorUniversalHingeJointElement
 
 controller = np.random.random((1, 2))
 
-element = TouchSensorHingeJointElement(controller)
+element = TouchSensorUniversalHingeJointElement(controller)
 polybot = AGGREGATE(2)
 
-sim = pyrosim.Simulator( play_paused=True, eval_time = 400, dt = .01 )
+sim = pyrosim.Simulator(eval_steps=500, dt = .01, play_paused=True, draw_joints=True)
 
 polybot.send_to_sim(sim, element)
 
