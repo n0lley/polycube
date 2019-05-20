17f0b3f6fc0b157c45fdb038f59e538bda5425b0
jack <jfelag@uvm.edu>
2019-05-20 13:12:46 -0400

    fleshing out coevolve methods

--- coevolve.py
+++ coevolve.py
@@ -1,9 +1,12 @@
+import numpy as np
 
 
 class COEVOLVE:
     '''
     COEVOLVE class that handles co-evolutionary methods
         for AGGREGATE and ELEMENT classes
+        
+    To be used with population class in ./population.py
     '''
     
     def __init__(self, pop1, pop2):
@@ -15,10 +18,47 @@ class COEVOLVE:
         self.elmts = pop2
         
     def exhaustive(self):
-        
+        '''
+        Evaluates every single aggregate composed with 
+            every single element 
+        '''
+
         for i in self.elmts.p:
             elmt = self.elmts.p[i]
             for j in self.aggrs.p:
                 aggr = self.aggrs.p[j]
+                aggr.send_to_sim(sim, elmt)
                 
-                aggr.build_with(elmt)
+    def random_subset(self, p, q):
+        '''
+        Selects subsets of proportion p from aggregate 
+            and proportion q from elements for evaluation
+        '''
+        
+        assert 0 <= p <= 1, print('First input needs to be a valid proportion')
+        assert 0 <= q <= 1, print('Second input needs to be a valid proportion')
+        
+        n1 = self.aggrs.popSize
+        n2 = self.elmts.popSize
+        
+        aggrSub = np.random.choice(n1, size=p*n1)
+        elmtSub = np.random.choice(n2, size=q*n2)
+        
+        for i in elmtSub:
+            elmt = self.elmts.p[i]
+            for j in aggrSub:
+                aggr = self.aggrs.p[j]
+                aggr.send_to_sim(sim, elmt)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+                
