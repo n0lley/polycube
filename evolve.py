69a1d98069db130b459cfd0286b64932af795f02
n0lley <ntolley13@gmail.com>
2019-06-03 15:38:37 -0400

    added script to save generations to saved_generations folder

--- evolve.py
+++ evolve.py
@@ -5,6 +5,8 @@ from element import ELEMENT
 from element import TouchSensorUniversalHingeJointElement
 from element import TouchAndLightSensorYAxisHingeJointElement
 from element import TouchAndLightSensorXAxisHingeJointElement
+import pickle
+import os
 
 elementTypes = [TouchSensorUniversalHingeJointElement,
                 TouchAndLightSensorYAxisHingeJointElement,
@@ -42,19 +44,13 @@ for g in range(1, GENS+1):
     #report fitness values
     coevolve.print_fitness(g)
     
-    
-    
-    
-    
-
-    
-
-
-
-
-
-
-
-
-
-
+    try:
+        if not os.path.exists('./saved_generations/'):
+            os.makedirs('./saved_generations/')
+        fileName = './saved_generations/gen'
+        f = open(fileName + format(g) + '.p', 'wb')
+        pickle.dump(coevolve, f)
+        f.close()
+        
+    except:
+        print ("Error saving generation", g, "to file.")
