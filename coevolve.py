e8b88b4e4aa9c2721816fa30c1ccb94f2b2c23a1
n0lley <ntolley13@gmail.com>
2020-01-07 17:22:48 -0500

    fixed error in print_fitness function

--- coevolve.py
+++ coevolve.py
@@ -164,6 +164,8 @@ class COEVOLVE:
                  if (np.isnan(fit) or np.isinf(fit) or len(self.elmts.p[i].scores)==0):
                      fit = 0
                  self.elmts.p[i].fitness = fit
+                 if [self.elmts.p[i].fitness] != self.elmts.p[i].scores[0:fpi]:
+                    print(fit, self.elmts.p[i].scores[0:fpi])
             except Exception as e:
                 print(e)
                 raise(e)
@@ -180,7 +182,7 @@ class COEVOLVE:
         for i in range(len(self.elmts.p)):
             if self.elmts.p[i].fitness > best:
                 best = self.elmts.p[i].fitness
-                besti = 0
+                besti = i
         print(besti, ":", best, self.elmts.p[besti].scores)
 
     def reset(self):
