2bf2ec2d876d72b89eafbb0360fb1078c6303c11
n0lley <ntolley13@gmail.com>
2020-02-05 16:33:28 -0500

    testing consistency of MPI evaluation

--- robot.py
+++ robot.py
@@ -9,26 +9,15 @@ import population
 
 np.random.seed(111)
 
-f = open("saved_generations/gen20.p", 'rb')
+f = open("saved_generations/gen200.p", 'rb')
 gen = pickle.load(f)
 f.close()
 co = gen[0]
 
-fit = 0
-beste = None
-for e in co.elmts.p:
-    print(e.scores)
-    if e.fitness > fit:
-        fit = e.fitness
-        beste = e
-        
-scores = []
-for a in co.aggrs.p:
-    sim = pyrosim.Simulator(eval_steps = 1000, play_blind=True, play_paused=False, dt=.01)
-    scores.append(a.evaluate(sim, beste, [0,0]))
-    print(len(scores))
-    
-scores.sort()
-fpi = math.ceil(len(scores)*.05)
-print(scores)
-print(beste.scores)
+co.elmts.p.Print()
+
+co.reset()
+
+co.exhaustive()
+
+co.elmts.p.Print()
